47f600aa8c487fe3edcd878c907a2c70f9dc181b
alecaselli <cas1312@ymail.com>
2023-08-02 19:55:59 +0200

    Aggiunto sistema cromatico per agevolare la lettura e la verifica

--- Componenti_shell/.ctrlGer.sh
+++ Componenti_shell/.ctrlGer.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+#definisco shorcut per i colori e i font
+#togliere 60 ai valori superiori al 90 per impostare i colori a basso contrasto
+def='\033[0m'
+orange='\033[33m\033[1m'
+green='\033[32m\033[1m'
+cyan='\033[96m\033[1m'
+red='\033[31m\033[1m'
+
 target=$1
 
-echo "Che lettera vuoi utilizzare?"
+echo -e "${orange}Che lettera vuoi utilizzare?${cyan}"
 read L
 
 cat .ctrlGer > /tmp/fileTemp
 sed -i s/{lettera}/$L/g /tmp/fileTemp
 cat < /tmp/fileTemp >> $target/FCP.sh
 
-echo "Controllo delle gerarchie inserito correttamente"
+echo -e "${green}Controllo delle gerarchie inserito correttamente"
 
--- Componenti_shell/.ctrlPar.sh
+++ Componenti_shell/.ctrlPar.sh
@@ -1,13 +1,30 @@
 #/!bin/bash
 
+#definisco shorcut per i colori e i font
+#togliere 60 ai valori superiori al 90 per impostare i colori a basso contrasto
+def='\033[0m'
+orange='\033[33m\033[1m'
+green='\033[32m\033[1m'
+cyan='\033[96m\033[1m'
+red='\033[31m\033[1m'
+
 target=$1
+
+while true
+do
+        #leggo da standard input il numero di parametri richiesto dall'utente
+        echo -e "${orange}Quanti sono i parametri minimi richiesti?${cyan}"
+        read P
+        
+        case $P in
+        *[!0-9]*) echo -e "${red}$P non numero o non corretto"
+		  continue ;;
+	*) break ;;
+	esac
+done
 
 #creo un file temporaneo ulteriore dove inserire il numero di parametri da inserire nel case
 >/tmp/modTemp
-
-#leggo da standard input il numero di parametri richiesto dall'utente
-echo "Quanti sono i parametri minimi richiesti?"
-read P
 
 #stampo all'interno di modtemp, senza andare a capo, 0 in quanto i parametri minimi non sono mai 0
 echo -n "0" >> /tmp/modTemp
